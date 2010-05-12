e349cbb9af6246f480395b9c143940dd1d8ca3e6
joe <joe@8fca1ad0-7d11-0410-afd2-b43cb0f742c7>
2010-05-12 00:09:46 +0000

    Updated installer/buildout for win32
    git-svn-id: http://xsd.alexandriaconsulting.com/repos/trunk/synthesis@540 8fca1ad0-7d11-0410-afd2-b43cb0f742c7

--- installer/install-linux.sh
+++ installer/install-linux.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 
 ## supports: debian, ubuntu, rhel, centos
 ## should run on most *nix platforms
--- installer/install-windows.bat
+++ installer/install-windows.bat
@@ -1,11 +1,28 @@
+@echo on
 
-## setup Python
-msiexec /i packages/python-2.5.msi TARGETDIR=C:\synthesis\python
-PYTHON_HOME = C:\synthesis\python
+Set InstallDir=C:\Synthesis
+mkdir "%InstallDir%\src"
+mkdir "%InstallDir%\Python25"
+mkdir "%InstallDir%\PostgreSQL"
+mkdir "%InstallDir%\GnuPG"
+xcopy /EQR "%CD%\buildout\src\synthesis" "%InstallDir%\src"
+xcopy /EQR "%CD%\buildout\packages\win32\Python25" "%InstallDir%\Python25"
+xcopy /EQR "%CD%\buildout\packages\win32\pythoncom25.dll" "C:\windows\system32\"
+xcopy /EQR "%CD%\buildout\packages\win32\pywintypes25.dll" "C:\windows\system32\"
+xcopy /EQR "%CD%\buildout\packages\win32\python25.dll" "C:\windows\system32\"
+xcopy /EQR "%CD%\buildout\packages\win32\msvcr71.dll" "C:\windows\system32\"
 
-## setup Postgresql server
+"%CD%\buildout\packages\win32\postgresql-8.3.10-1-windows.exe" --mode unattended --prefix %InstallDir%\PostgreSQL --superpassword mypassword
 
+"%CD%\buildout\packages\win32\gnupg-w32cli-1.4.10b.exe" /S /C="%CD%\buildout\packages\win32\gnupg.ini" /D=%InstallDir%\GnuPG
 
-## bootstrap
+cd buildout
+"%InstallDir%\Python25\python.exe" bootstrap.py
+"%CD%\bin\buildout"
 
-	
+copy "%CD%\bin\.exe" "%InstallDir%\src\python.exe"
+copy "%CD%\bin\-script.py" "%InstallDir%\src\python-script.py"
+copy "%InstallDir%\GnuPG\gpg.exe" "%InstallDir%\src\"
+
+set PGPASSWORD=mypassword
+"%InstallDir%\PostgreSQL\bin\createdb.exe" -U postgres synthesis

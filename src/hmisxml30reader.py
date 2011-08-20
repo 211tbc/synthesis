1c01dae63cc2aa91fd8366f3a489394e63275ded
eric <eric@8fca1ad0-7d11-0410-afd2-b43cb0f742c7>
2011-08-20 20:07:49 +0000

    uncommenting line in hmisxmlreader, the absence of which was causing it to err.  no idea why I ever commented it.
    git-svn-id: http://xsd.alexandriaconsulting.com/repos/trunk/synthesis@818 8fca1ad0-7d11-0410-afd2-b43cb0f742c7

--- src/hmisxml30reader.py
+++ src/hmisxml30reader.py
@@ -28,7 +28,7 @@ class HMISXML30Reader:
 
     def __init__(self, xml_file, db):
         ''' Put XML file into local object '''
-        #self.xml_file = xml_file
+        self.xml_file = xml_file
         #if settings.DEBUG:
         #    print "does self.xml_file exist?", os.path.exists(self.xml_file)
         ''' Instantiate database object '''

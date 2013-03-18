967d04a86e029eb79c5066f90a95e781ddcbecf7
eric <eric@8fca1ad0-7d11-0410-afd2-b43cb0f742c7>
2013-03-18 16:15:04 +0000

    adding tbc tests
    git-svn-id: http://xsd.alexandriaconsulting.com/repos/trunk/synthesis@1035 8fca1ad0-7d11-0410-afd2-b43cb0f742c7

---
+++ src/tests/tbctest1.py
@@ -0,0 +1,26 @@
+import urllib2
+import base64
+from Encryption import *
+import conf.settings
+
+tbc = open('test_files/HUD_HMIS_TBC.xml', 'r')
+xml = tbc.read()
+tbc.close()
+gpg = GPG()
+encrypted_xml = gpg.encrypt(xml)
+encoded_xml = base64.b64encode(encrypted_xml)
+
+data="""--98f3d0e0-8336-489b-b815-a48da207a689
+Content-Disposition: attachment; name="tbctest"; filename="tbctest.xml"
+Content-Type: text/xml
+
+%s""" % (encoded_xml, )
+hs = urllib2.HTTPSHandler(debuglevel=1)
+opener = urllib2.build_opener(hs)
+urllib2.install_opener(opener)
+#request = urllib2.Request('https://pix.penguix.net:8025/docs')
+request = urllib2.Request('http://127.0.0.1:5001/docs')
+request.add_header("Content-Type", "multipart/form-data; boundary=98f3d0e0-8336-489b-b815-a48da207a689")
+request.add_header("User-Agent", "synthesis")
+request.add_data(data)
+print urllib2.urlopen(request).read()

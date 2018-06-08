7213bc2ed2f8cdceefdacb40089411843d990c28
Frederick <yd.adept@gmail.com>
2018-06-08 18:25:38 -0400

    Changed lastdatetime.py to not cause an exception when starting synthesis.

--- src/lastdatetime.py
+++ src/lastdatetime.py
@@ -19,17 +19,20 @@ SMTPRECIPIENTS = {
     }
 }
 
-# Assume that record exists so 
-lifecycle_event = session.query(LastDateTime).filter(LastDateTime.event == 'file received').first()
-event_date_time = lifecycle_event.event_date_time
-time_elapsed = (datetime.datetime.now() - event_date_time)
-if time_elapsed.days >= 3:
-    settings.SMTPAUTH = True
-    #settings.SMTPTLS = True
-    smtp = smtpInterface(settings)
-    smtp.setMessageSubject("Warning: No referrals received")
-    smtp.setRecipients(SMTPRECIPIENTS['synthesis'])
-    text = "You are receiving this message because synthesis has not received an incoming referral in %d days.\r\n" % time_elapsed.days
-    smtp.setMessage("%s\r\n" % text)
-    smtp.sendMessage()
-    settings.SMTPAUTH = False
+try:
+    # Assume that record exists so 
+    lifecycle_event = session.query(LastDateTime).filter(LastDateTime.event == 'file received').first()
+    event_date_time = lifecycle_event.event_date_time
+    time_elapsed = (datetime.datetime.now() - event_date_time)
+    if time_elapsed.days >= 3:
+        settings.SMTPAUTH = True
+        #settings.SMTPTLS = True
+        smtp = smtpInterface(settings)
+        smtp.setMessageSubject("Warning: No referrals received")
+        smtp.setRecipients(SMTPRECIPIENTS['synthesis'])
+        text = "You are receiving this message because synthesis has not received an incoming referral in %d days.\r\n" % time_elapsed.days
+        smtp.setMessage("%s\r\n" % text)
+        smtp.sendMessage()
+        settings.SMTPAUTH = False
+except:
+    pass
